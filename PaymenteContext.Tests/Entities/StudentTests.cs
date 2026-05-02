61c0dd04254d54efb9883174089ee00e9a0caa81
Roger de Paula <roger.paula@chipcia.com.br>
2026-05-02 14:42:24 -0300

    chore: substituir MSTest por xUnit no projeto de testes
    Renomeia o projeto de testes para PaymenteContext.Tests, troca o motor
    de MSTest para xUnit (com coverlet.collector) e atualiza a solução com
    o novo GUID do projeto.

---
+++ PaymenteContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,12 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymenteContext.Tests.Entities;
+
+public class StudentTests
+{
+    [Fact]
+    public void Should_Create_Student()
+    {
+        // var student = new Student()
+    }
+}
