cb064b12613290149d8a75c5e36907b40240d1a7
Roger de Paula <roger.paula@chipcia.com.br>
2026-05-02 02:21:08 -0300

    feat: adicionar value object Address

---
+++ PaymentContext.Domain/ValueObjects/Address.cs
@@ -0,0 +1,21 @@
+using PaymentContext.Shared.ValueObjects;
+
+namespace PaymentContext.Domain.ValueObjects;
+
+public class Address(
+    string street,
+    string number,
+    string neighborhood,
+    string city,
+    string state,
+    string country,
+    string zipCode) : ValueObject
+{
+    public string Street { get; private set; } = street;
+    public string Number { get; private set; } = number;
+    public string Neighborhood { get; private set; } = neighborhood;
+    public string City { get; private set; } = city;
+    public string State { get; private set; } = state;
+    public string Country { get; private set; } = country;
+    public string ZipCode { get; private set; } = zipCode;
+}
