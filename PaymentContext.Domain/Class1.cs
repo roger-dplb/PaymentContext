5b2b8356ac620218bb6d9fc9bb6008bc29957e00
Roger de Paula <roger.paula@chipcia.com.br>
2026-05-02 00:32:59 -0300

    initial commit

---
+++ PaymentContext.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace PaymentContext.Domain;
+
+public class Class1
+{
+}
---
+++ PaymentContext.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentContext.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
